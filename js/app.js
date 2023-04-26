9ba371b33d1a14611147cd6c68cded8a0af05fcf
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-04-26 12:43:19 +0300

    Writing the Skills part

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-			navClose = document.getElementById('nav-close');
+ navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
 const navLink = document.querySelectorAll('.nav_link')
 
 if(navToggle) {
@@ -22,3 +22,23 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+	let itemClass = this.parentNode.className
+
+	for(i = 0; i < skillsContent.length; i++) {
+		skillsContent[i].className = 'skills_content skills_close'
+	}
+	if(itemClass === 'skills_content skills_close') {
+		this.parentNode.className = 'skills_content skills_open'
+	}
+}
+
+skillsHeader.forEach((el) => {
+	el.addEventListener('click', toggleSkills)
+})
+
