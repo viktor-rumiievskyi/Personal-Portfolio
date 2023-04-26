076c0573c187c5c0d33d397a29f6131728c5026b
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-04-26 18:26:36 +0300

    Writing the Portfolio part

--- js/app.js
+++ js/app.js
@@ -85,3 +85,21 @@ modalClose.forEach((modalClose) => {
 	})
 })
 // Services
+
+// Portfolio
+let swiper = new Swiper('.portfolio_container', {
+	cssMode: true,
+	loop: true,
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
+
+
+// Portfolio
