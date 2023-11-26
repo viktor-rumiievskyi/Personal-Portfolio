22d8696a9ca30380774b843e9b470fc674757369
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-11-26 20:15:10 +0200

    Fixed bugs, added new projects, changed cursor style

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+var cursor = document.querySelector('.cursor'),
+cursorScale = document.querySelectorAll('.cursor-scale'),
+    mouseX = 0,
+    mouseY = 0
+
+gsap.to({}, 0.016, {
+    repeat: -1,
+
+    onRepeat: function () {
+        gsap.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        })
+    }
+});
+
+window.addEventListener("mousemove", function (e) {
+	mouseX = e.clientX;
+	mouseY = e.clientY
+});
+
+
+cursorScale.forEach(link => {
+	link.addEventListener('mouseleave', () => {
+		cursor.classList.remove('grow');
+		cursor.classList.remove('grow-small');
+	});
+	link.addEventListener('mousemove', () => {
+		cursor.classList.add('grow');
+		if(link.classList.contains('small')){
+			cursor.classList.remove('grow');
+			cursor.classList.add('grow-small');
+		}
+	});
+})
+
+
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close')
@@ -168,3 +207,28 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// var cursor = document.querySelector('.cursor'),
+// mouseX = 0,
+// mouseY = 0
+
+// gsap.to({}, 0.016, {
+// 	repeat: -1,
+
+// 	onRepeat: function() {
+// 		gsap.set(cursor, {
+// 			css: {
+// 				left: mouseX,
+// 				top: mouseY
+// 			}
+// 		})
+// 	}
+// });
+
+// window.addEventListener("mousemove", function (e){
+// 	mouseX = e.clientX;
+// 	mouseY = e.clientY
+// })
+
+
+
