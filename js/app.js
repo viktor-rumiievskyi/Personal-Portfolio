09a4e7d890fbab7de5051bcf101114daa6506ece
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-12-18 22:38:10 +0200

    Added new projects

--- js/app.js
+++ js/app.js
@@ -208,27 +208,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-// var cursor = document.querySelector('.cursor'),
-// mouseX = 0,
-// mouseY = 0
-
-// gsap.to({}, 0.016, {
-// 	repeat: -1,
-
-// 	onRepeat: function() {
-// 		gsap.set(cursor, {
-// 			css: {
-// 				left: mouseX,
-// 				top: mouseY
-// 			}
-// 		})
-// 	}
-// });
-
-// window.addEventListener("mousemove", function (e){
-// 	mouseX = e.clientX;
-// 	mouseY = e.clientY
-// })
 
 
 
